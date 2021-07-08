d1c1cfdb32eac5b3e12cf9a4286b3828889d99e5
ScottSong0714 <76896600+ScottSong0714@users.noreply.github.com>
2021-07-09 01:52:07 +0800

    7.9更新
    删除了计时器2，重做了计时器1以及计时器3

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -21,8 +21,9 @@ namespace WindowsFormsApp1
      */
     public partial class 匹配小游戏 : Form
     {
-        //计时器
-        int time = 0;
+        //全局计时器
+        DateTime TimeNow = new DateTime();
+        TimeSpan TimeCount = new TimeSpan();
 
         Random random = new Random();
         //添加图标（利用Webding字体生成图标，每个字母对应一个图标）
@@ -45,6 +46,7 @@ namespace WindowsFormsApp1
             InitializeComponent();
             giveIcon();//若显示图标，调用方法giveIcon()
             timer3.Start();//开启全局计时
+            TimeNow = DateTime.Now;
         }
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
@@ -55,6 +57,8 @@ namespace WindowsFormsApp1
         /**
          * giveIcon()函数：
          * 用于随机分配图标的函数。
+         * 该函数遍历TableLayoutPanel中的每个label控件，并对每个控件
+         * 执行相同的语句。
          */
         private void giveIcon()
         {
@@ -78,15 +82,17 @@ namespace WindowsFormsApp1
         private void 匹配小游戏_Load(object sender, EventArgs e)
         {
             this.Text = "找相同";
+            this.timer3.Interval = 1000;
         }
 
         /**
          * label_Click()函数：
-         * 
+         * 此函数用于点击label之后产生的显现图标的效果，
+         * 以及检查两图标是否匹配，调用调用CheckForWinner()程序，
+         * 以确认玩家是否胜利。
          */
         private void label_Click(object sender, EventArgs e)
         {
-
             //只有当两个不匹配的图标出现时才计时，所以在计时器工作时要忽视任何点击
             if (timer1.Enabled == true)
                 return;
@@ -97,19 +103,18 @@ namespace WindowsFormsApp1
             {
                 if (clickedLabel.ForeColor == Color.Black)
                     return;
-
+                
+                //clickedLabel.ForeColor = Color.Black;
                 if (firstClicked == null)
                 {
                     firstClicked = clickedLabel;
                     firstClicked.ForeColor = Color.Black;
-                    timer2.Start();
                     return;
                 }
 
                 secondClicked = clickedLabel;
                 secondClicked.ForeColor = Color.Black;
                 timer1.Start();
-                timer2.Stop();
 
                 //调用CheckForWinner()程序，检查玩家是否胜利
                 CheckForWinner();
@@ -120,16 +125,22 @@ namespace WindowsFormsApp1
                     firstClicked = null;
                     secondClicked = null;
 
-                    //关闭定时器1和定时器2
+                    //关闭定时器
                     timer1.Stop();
-                    timer2.Stop();
 
                     return;
                 }
                 
             }
         }
-        
+
+        /**
+         * timer1_Tick_1(object sender, EventArgs e)函数：
+         * 计时器1工作的函数。
+         * 调用Stop()停止计时器，是两个引用变量firstClicked
+         * 和secondClicked来捕捉玩家点击的两个标签，并使标签不可见。
+         * 然后将firstClicked和secondClicked引用变量重置为null。
+         */
         private void timer1_Tick_1(object sender, EventArgs e)
         {
             //停止计时
@@ -137,41 +148,27 @@ namespace WindowsFormsApp1
 
             //隐藏前一个图标
             firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
 
-            //重置firstClicked和secondClicked
-            firstClicked = secondClicked;
-
-            timer2.Start();
-
+            firstClicked = null;
             secondClicked = null;
-        }
-
-        private void timer2_Tick(object sender, EventArgs e)
-        {
-            timer2.Stop();
-            // timer1.Stop();
-            if (secondClicked == null)
-            {
-               
-                //隐藏前一个图标
-                firstClicked.ForeColor = firstClicked.BackColor;
-
-                //重置firstClicked
-                firstClicked = null;
-
-                return;
-            }
         }
 
         private void timer3_Tick(object sender, EventArgs e)
         {
-            time++;
+            TimeCount = DateTime.Now - TimeNow;
         }
 
+        /**
+         * CheckForWinner()函数：
+         * 该函数使用foreach遍历每个图标，使用相等运算符检查每个图标
+         * 颜色是否匹配。若匹配，图标将保持不可见，玩家还没有匹配所有
+         * 剩余图标。这种情况下，将使用return语句跳过其余函数。如果遍
+         * 历所有标签而不执行return语句，则意味着所有图标匹配。程序显
+         * 示MessageBox，调用Close()结束游戏。
+         */
         private void CheckForWinner()
         {
-            int minus = 0;
-            int seconds = 0;
             //foreach遍历每个图标,获取控件内的集合元素
             foreach (Control control in tableLayoutPanel1.Controls)
             {
@@ -185,14 +182,11 @@ namespace WindowsFormsApp1
 
             //停止计时
             timer1.Stop();
-            timer2.Stop();
             timer3.Stop();
-
-
-            minus = time / 600;
-            seconds = (time % 600) / 10;
+            
             //如果没有跳出循环，则说明没有找到不匹配的图标，说明玩家赢了
-            MessageBox.Show("恭喜您，成功通过游戏。您所用时为：" + minus.ToString() + "分" + seconds.ToString() + "秒");
+            MessageBox.Show("恭喜您，成功通过游戏。您所用时为："
+                + string.Format("{0:00}:{1:00}:{2:00}", TimeCount.Hours, TimeCount.Minutes, TimeCount.Seconds));
             Close();//结束游戏
         }
 
